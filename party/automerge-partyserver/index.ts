794ffc83b724368a8cc4efbefa0eff6c9163363c
chee <yay@chee.party>
2024-08-08 23:47:29 +0100

    instantiate repo in onConnect

--- party/automerge-partyserver/index.ts
+++ party/automerge-partyserver/index.ts
@@ -1,4 +1,4 @@
-import { type PeerId, Repo, } from '@automerge/automerge-repo';
+import { type PeerId } from '@automerge/automerge-repo/slim';
 import { DurableObjectState } from '@cloudflare/workers-types';
 import { Connection, routePartykitRequest, Server } from 'partyserver';
 import { WorkerWebSocketAdapter } from '../automerge-repo-network-websocket/WorkerWebSocketAdapter';
@@ -10,18 +10,8 @@ const wsReadyStateClosing = 2; // eslint-disable-line
 const wsReadyStateClosed = 3; // eslint-disable-line
 
 export class AutomergeServer extends Server {
-
-	readonly repo: Repo;
-
-	constructor(
-		private readonly ctx: DurableObjectState,
-		private readonly env
-	) {
+	constructor(private readonly ctx: DurableObjectState, private readonly env) {
 		super(ctx, env);
-		this.repo = new Repo({
-			storage: new DurableObjectStorageAdapter(ctx.storage),
-			peerId: `worker-${ctx.id}` as PeerId,
-		});
 	}
 
 	// eslint-disable-next-line @typescript-eslint/require-await
@@ -33,29 +23,44 @@ export class AutomergeServer extends Server {
 	async onSave(): Promise<void> {}
 
 	async onStart(): Promise<void> {
-		console.log("on Start");
+		console.log('on Start');
 
-    const src = await this.onLoad();
-    if (src != null) {
-      // apply update to local document
-    }
+		const src = await this.onLoad();
+		if (src != null) {
+			// apply update to local document
+		}
 
 		// debounced save on document update
-  }
+	}
 
 	async onConnect(conn: Connection) {
 		const ctx = this.ctx;
-		console.log("connecting", conn.id);
-		this.repo.networkSubsystem.addNetworkAdapter(new WorkerWebSocketAdapter(conn));
+		console.log('connecting hoot', conn.id);
+		const { Repo } = await import('@automerge/automerge-repo');
+		const repo = new Repo({
+			network: [new WorkerWebSocketAdapter(conn)],
+			storage: new DurableObjectStorageAdapter(ctx.storage),
+			peerId: `worker-${ctx.id}` as PeerId,
+		});
 	}
 
 	onMessage(sender: Connection<unknown>, message) {
-		console.log("onMessage", sender.id, message, [...this.getConnections()].map(c => c.id));
+		console.log(
+			'onMessage',
+			sender.id,
+			message,
+			[...this.getConnections()].map(c => c.id)
+		);
 
 		this.broadcast(message, [sender.id]);
 	}
 
-	onClose(conn: Connection<unknown>, code: number, reason: string, wasClean: boolean): void | Promise<void> {
+	onClose(
+		conn: Connection<unknown>,
+		code: number,
+		reason: string,
+		wasClean: boolean
+	): void | Promise<void> {
 		closeConn(conn);
 	}
 }
@@ -66,28 +71,31 @@ function closeConn(conn: Connection) {
 	try {
 		conn.close();
 	} catch (e) {
-		console.warn("failed to close connection", e);
+		console.warn('failed to close connection', e);
 	}
 }
 
 function send(conn: Connection, m: Uint8Array) {
-  if (
-    conn.readyState !== undefined &&
-    conn.readyState !== wsReadyStateConnecting &&
-    conn.readyState !== wsReadyStateOpen
-  ) {
-    closeConn(conn);
-  }
-  try {
-    conn.send(m);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (e) {
-    closeConn(conn);
-  }
+	if (
+		conn.readyState !== undefined &&
+		conn.readyState !== wsReadyStateConnecting &&
+		conn.readyState !== wsReadyStateOpen
+	) {
+		closeConn(conn);
+	}
+	try {
+		conn.send(m);
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (e) {
+		closeConn(conn);
+	}
 }
 
 export default {
 	fetch(request, env) {
-		return routePartykitRequest(request, env) || new Response('Not Found', { status: 404 });
+		return (
+			routePartykitRequest(request, env) ||
+			new Response('Not Found', { status: 404 })
+		);
 	},
 };
